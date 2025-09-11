cabe8543e6e1d3b61cca781db2b90fab42e653af
HanshithE <234g1a3342@srit.ac.in>
2025-09-11 15:40:18 +0530

    first commit

---
+++ CompoundIntrest.js
@@ -0,0 +1,5 @@
+let Principle = 10;
+let time = 5, rateintrest = 6;
+
+console.log("compound Interest: "+(Principle * (1 + rateintrest)^time))
+let input = promt("enter no:")
---
+++ new1.js
@@ -0,0 +1,7 @@
+function generateWelcomeMessage(name){
+    return "Welcome, "+ name + "! Good morning";
+}
+console.log(generateWelcomeMessage("Bob"))
+
+let  a = 10;
+console.log(++a)
